ff5e98f01490419fb8f89b81dd30f66aa71965eb
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2022-08-28 00:41:40 +0530

    Added poster images of each movie and updated styles

--- App/screens/PopularMoviesScreen.js
+++ App/screens/PopularMoviesScreen.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-import { StyleSheet, View, Text, TouchableOpacity,FlatList, SafeAreaView} from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity,FlatList, SafeAreaView, ActivityIndicator, Image} from "react-native";
 export default class PopularMoviesScreen extends Component{
   constructor(){
     super();
@@ -8,6 +8,7 @@ export default class PopularMoviesScreen extends Component{
       movieList:[],
     }
   };
+
   componentDidMount(){
     this.setState({isFetching:true}, ()=>{
       fetch('https://api.themoviedb.org/3/movie/popular?api_key=c4669cfb558067fec6d01ab4552a4c26&language=en-US&page=1',{
@@ -17,7 +18,8 @@ export default class PopularMoviesScreen extends Component{
       }
     }).then(res => res.json())
     .then(res => this.setState({isFetching:false, movieList:res.results}))
-    })
+    .catch((error) =>{(console.error(error))})
+    });
   }
 
   moreDetails=()=>{
@@ -26,50 +28,81 @@ export default class PopularMoviesScreen extends Component{
 
   render(){
     // movie items are loaded with title, overview and poster
-    const Item = ({ title , overview }) => (
+    const Item = ({ title , overview , poster_path}) => (
         <TouchableOpacity onPress={this.moreDetails} style={styles.item}>
           <Text style={styles.title}>{title}</Text>
-          <Text style={styles.title}>{overview}</Text>
+          <Image style={styles.image} source={{uri:"https://image.tmdb.org/t/p/w500/"+poster_path}}/>
+          <Text style={styles.textStyle}>{overview}</Text>
         </TouchableOpacity>
       );
+
       // render each movie item
     const renderItem = ({ item}) => (
-        <Item title={item.title} 
-        overview={item.overview}/>
-        
+        <Item title={item.title}
+        poster_path={item.poster_path}
+        overview={item.overview}/> 
       );
     
     const {movieList, isFetching}= this.state;
-    // const message = isFetching? 'Connecting': movieList.title;
-    return(
+    if(isFetching){
+        return(
+            <View style={styles.container}>
+              <ActivityIndicator/>
+            </View>
+          );
+    }else{
+        return(
         <SafeAreaView style={styles.container}>
-            <FlatList
-                data={movieList}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}/>
+            <FlatList 
+            data={movieList}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}/>    
         </SafeAreaView>
-    )
+    )}   
   }
-}
+};
 
 const styles = StyleSheet.create({
     container:{
       flex:1,
       justifyContent:'center',
-      alignItems:'center'
+      alignItems:'center',
+      backgroundColor:"#ddd"
     },
     item: {
         flex:1,
+        backgroundColor:"#fff",
+        padding: 20,
+        margin:20,
+        borderRadius:20,
         alignSelf:'stretch',
-        margin:10,
         alignItems:'center',
-        justifyContent:'center',
-        borderBottomWidth:2,
-        // borderBottomColor:'#eee',
-        // borderRadius:4
+        justifyContent:'center',   
     },
     title: {
         fontSize: 28,
+        color:'black',
+        marginBottom:5,
+    },
+    textStyle:{
+        fontSize:15,
         color:'black'
-      },
+    },
+    image:{
+        width:300,
+        height:320,
+        resizeMethod:'auto',
+        resizeMode:'contain',
+        alignContent:'center',    
+        // borderRadius:10,
+        borderWidth:0.5,
+        // borderColor:'#807760',
+        // borderTopLeftRadius: 20,
+        // borderTopRightRadius: 20,
+        // borderBottomRightRadius:20,
+        // borderBottomLeftRadius:20,
+        padding: 20,
+        margin:10,
+        borderRadius:20,
+    }
   })
