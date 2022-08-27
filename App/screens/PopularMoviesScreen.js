1ab4702b068cb8cd80f21f93667c20f6252addab
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2022-08-27 22:03:03 +0530

    tested API with a console log

--- App/screens/PopularMoviesScreen.js
+++ App/screens/PopularMoviesScreen.js
@@ -1,26 +1,31 @@
-import { NavigationHelpersContext } from "@react-navigation/native";
 import React,{Component} from "react";
-import { StyleSheet, View , Text, Button} from "react-native";
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView,} from "react-native";
 
 export default class PopularMoviesScreen extends Component{
-    onPressButton =()=>{this.props.navigation.navigate('Details')};
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text style={styles.textStyle}>Popular Movies Screen</Text>
-                <Button onPress={this.onPressButton} title="Click Me"/>
-            </View>
-        )
+  constructor(){
+    super();
+    this.state= {
+      isFetching:true,
+      movieList:null,
     }
-}
+  };
+  componentDidMount(){
+    this.setState({isFetching:true}, ()=>{
+      fetch('https://api.themoviedb.org/3/movie/popular?api_key=c4669cfb558067fec6d01ab4552a4c26&language=en-US&page=1',{
+      method:'GET',
+      headers:{
+        Accept : 'application/json',
+      }
+    }).then(res => res.json())
+    .then(res => console.log(res.results))
+    })
+  }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    textStyle:{
-        fontSize:28,
-    }
-})
+  render(){
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    )
+  }
+}
