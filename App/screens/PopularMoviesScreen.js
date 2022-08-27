210168768adfe8afe2ecc0be5ee316b80c5e1932
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2022-08-27 21:40:36 +0530

    Created navigation between the two screens through a button

--- App/screens/PopularMoviesScreen.js
+++ App/screens/PopularMoviesScreen.js
@@ -1,11 +1,14 @@
+import { NavigationHelpersContext } from "@react-navigation/native";
 import React,{Component} from "react";
-import { StyleSheet, View , Text} from "react-native";
+import { StyleSheet, View , Text, Button} from "react-native";
 
 export default class PopularMoviesScreen extends Component{
+    onPressButton =()=>{this.props.navigation.navigate('Details')};
     render(){
         return(
             <View style={styles.container}>
                 <Text style={styles.textStyle}>Popular Movies Screen</Text>
+                <Button onPress={this.onPressButton} title="Click Me"/>
             </View>
         )
     }
