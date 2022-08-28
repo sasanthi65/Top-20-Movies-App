0ecc37bfe31c84422302247b77520720266ce243
Sasanthi Lakmini <33154236+sasanthi65@users.noreply.github.com>
2022-08-28 19:04:10 +0530

    Loaded poster image to the details screen

--- App/screens/DetailedScreen.js
+++ App/screens/DetailedScreen.js
@@ -1,20 +1,20 @@
 import React,{Component} from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 
 export default class DetailedScreen extends Component{
     render(){
         const name = this.props.route.params.title;
         const overview = this.props.route.params.overview;
+        const poster_path = this.props.route.params.poster_path;
         return(
             <View style={styles.container}>
                 <Text style={styles.title}> {name}</Text>
+                <Image style={styles.image} source={{uri:"https://image.tmdb.org/t/p/w500/"+poster_path}}></Image>
                 <Text style={styles.textStyle}>{overview}</Text>
-            </View>
-            
+            </View>    
         );
-    }
-    
+    }  
 }
 
 
@@ -25,9 +25,22 @@ const styles = StyleSheet.create({
         alignItems:'center',
     },
     title:{
+        marginTop:3,
+        alignItems:'flex-start',
         fontSize:28
     },
     textStyle:{
         fontSize:14,
+    },
+    image:{
+        width:300,
+        height:320,
+        resizeMethod:'auto',
+        resizeMode:'contain',
+        alignContent:'center',    
+        borderWidth:0.5,
+        padding: 20,
+        margin:10,
+        borderRadius:20,
     }
 })
